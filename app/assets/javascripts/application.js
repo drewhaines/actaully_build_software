b3812de852bb4a38547e4a6296f0086de2453635
Andrew Haines <drewhaines@yahoo.com>
2015-09-20 09:32:33 -0700

    add course images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function(){
+    var x = 0;
+
+    $('.menu_icon').unbind().click(function() {
+        if(x==0) {
+            $('.menu_hidden').show();
+            x = 1;
+        }else{
+            $('.menu_hidden').hide();
+            x = 0;
+        }
+    });
+
+
+
+});
