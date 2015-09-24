fdec3d0a9e1af4318f3551b72ee2e76c1fcf4b8a
Andrew Haines <drewhaines@yahoo.com>
2015-09-23 18:40:09 -0700

    add users

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :check_admin, only: [:index, :create, :edit, :update, :destroy]
 
   # GET /courses
   # GET /courses.json
@@ -71,5 +72,11 @@ class CoursesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
       params.require(:course).permit(:title, :description, :feature_img, :small_imgage, :small_dark_imgage)
+    end
+
+    def check_admin
+      unless current_user.admin
+        redirect_to root_path and return
+      end
     end
 end
